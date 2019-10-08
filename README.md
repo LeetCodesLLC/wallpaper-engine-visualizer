37b22f403ad40cec43ed294b2580570c5e0473e8
Lennart Bader <mail@lbader.de>
2019-10-08 21:18:40 +0200

    Screenshots, License and Credits

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wallpaper Engine Visualizer
+## Features
+* Audio Visualizer
+* Clock
+* Spotify Integration
+* Particle Background
+
+## Screenshots
+<img src="screenshots/01.png" width="400" />
+<img src="screenshots/02.png" width="400" />
+<img src="screenshots/03.png" width="400" />
+<img src="screenshots/04.png" width="400" />
+<img src="screenshots/05.png" width="400" />
--- windows/credits-menu.js
+++ windows/credits-menu.js
@@ -5,7 +5,7 @@ $(function() {
             headerTitle: "Credits",
             content: `
                 <div class="container">
-                    <h3>&copy 2019 - Lennart Bader</h3>
+                    <h3>&copy 2019 - Kryptur</h3>
                     <p>
                         This Wallpaper has been created by Lennart Bader.
                     </p>
@@ -13,15 +13,18 @@ $(function() {
                         Used Libraries:
                         <ul class="browser-default">
                             <li>jQuery</li>
+                            <li>jQuery UI</li>
+                            <li>Arthesian Library</li>
                             <li>ParticlesJS</li>
                             <li>Moment JS</li>
                             <li>Create JS</li>
-                            <li>Tweenlite</li>
+                            <li>Tweenlite / Greensock</li>
                             <li>Smooth JS</li>
                             <li>Materialize</li>
                             <li>Google Material Icons</li>
-                            <li>Arthesian Library</li>
-                            <li>jsPanel</li>
+                            <li>PickR</li>
+                            <li>Spotify-Web-API</li>
+                            <li>Keyboard JS</li>
                         </ul>
                     </p>
                 </div>
